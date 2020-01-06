97137a5abe458378ade010e22f00f1fd1ed44ecc
Cyril5169 <516974551@qq.com>
2020-01-06 16:34:57 +0800

    1

--- src/Components/certificate.vue
+++ src/Components/certificate.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-import { GetYlContractByCustomer } from "@/api/card";
+import { GetCardByCustomer, GetYlContractByCustomer } from "@/api/card";
 
 export default {
   name: "certificate",
@@ -21,6 +21,7 @@ export default {
       area1: "",
       area2: "",
       area3: "",
+      businesslicenseNO: "",
       startDate: "",
       endDate: "",
       recordTitle: "", //根据评审记录的上面文字来判断协议书是否已通过
@@ -48,27 +49,32 @@ export default {
       let me = this;
       this.imgLoading = true;
       let name = this.name;
+      let buNo = this.businesslicenseNO;
       let area = this.area;
       let startDate = this.formatDate(this.startDate);
       let endDate = " - " + this.formatDate(this.endDate);
+      let date = startDate + endDate;
       var picture = this.$refs.picture,
         ctx = picture.getContext("2d"),
         img = new Image();
-      img.src = "http://14.29.221.109:10250/upload/images/newshouquan2.png";
+
       img.onload = function() {
         let x = 600 / img.width;
         let y = 800 / img.height;
         ctx.scale(x, y);
         ctx.drawImage(img, 0, 0);
-        ctx.font = "bold 64px serif";
+        ctx.font = "bold 64px 宋体";
         ctx.style = "black";
-        let namex = ((345 - name.length * 19) / 2 + 110) * 3.25;
-        ctx.fillText(name, namex, 1600);
-        ctx.fillText(area, 650, 1850);
-        ctx.fillText(startDate, 775, 1970);
-        ctx.fillText(endDate, 1125, 1970);
+        let namex = ((500 - name.length * 19) / 2 + 150) * 3.25;
+        ctx.fillText(name, namex, 1535);
+        let buNox = ((350 - buNo.length * 19) / 2 + 270) * 3.25;
+        ctx.fillText(buNo, buNox, 1635);
+        ctx.fillText(area, 650, 1985);
+        ctx.fillText(date, 775, 2105);
         me.imgLoading = false;
       };
+      //img.src = "http://14.29.221.109:10250/upload/images/newshouquan2.png";
+      img.src = "http://14.29.221.109:10250/upload/images/newcertificate.jpg";
     }
   },
   created() {
@@ -83,16 +89,21 @@ export default {
         if (this.recordTitle == "协议书通过") {
           this.showCer = true;
 
-          this.$axios
-            .post("/yulan/customerInfo/getAuthorization.do", {
-              //获取授权区域并展示canvas
-              cid: this.$store.state.user.data.customerMainId
-            })
-            .then(res => {
-              if (res.data != null) {
-                this.area3 = res.data.areaDistrict3Text;
-                this.area2 = res.data.areaDistrict2Text;
-                this.area1 = res.data.districtText;
+          // this.$axios
+          //   .post("/yulan/customerInfo/getAuthorization.do", {
+          //     //获取授权区域并展示canvas
+          //     cid: this.$store.state.user.data.customerMainId
+          //   })
+          GetCardByCustomer({
+            cid: this.$store.state.user.data.customerMainId,
+            year: this.$store.state.year
+          })
+            .then(res2 => {
+              if (res2.data != null && res2.data.length > 0) {
+                this.area3 = res2.data[0].areaDistrict3Text;
+                this.area2 = res2.data[0].areaDistrict2Text;
+                this.area1 = res2.data[0].districtText;
+                this.businesslicenseNO = res2.data[0].file2BusinesslicenseNo;
                 if (this.area2 == null) this.area2 = "";
                 if (this.area3 == null) this.area3 = "";
 
@@ -105,9 +116,9 @@ export default {
                   cid: this.$store.state.user.data.customerMainId,
                   year: this.$store.state.year
                 })
-                  .then(res => {
-                    let startDate = res.data.contract.startDate;
-                    let endDate = res.data.contract.endDate;
+                  .then(res3 => {
+                    let startDate = res3.data.contract.startDate;
+                    let endDate = res3.data.contract.endDate;
                     this.startDate = new Date(startDate);
                     this.endDate = new Date(endDate);
 
@@ -133,20 +144,12 @@ export default {
 <style scoped>
 .wrapper-cer {
   width: 100%;
-  height: 100%;
   height: 800px;
   background-color: white;
 }
 .picture {
   display: block;
   margin: 0 auto;
-  /* width: 600px; */
-  /* height:100%; */
-  /* background-color: #F00; */
-  /* background-image:url('../assets/images/授权书.jpg'); */
-  /* background-size: contain;
-    background-repeat: no-repeat;
-    position: relative; */
 }
 .name {
   width: 246px;
